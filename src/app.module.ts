3040f31a7acb144d585909b50d9f3b92d46613d1
toseef-ahmd <tauseefahmed92b@gmail.com>
2024-02-09 22:25:33 +0200

    fixed merge issues

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';}
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 import { ChatModule } from './chat/chat.module';
 import { UserModule } from './user/user.module';
 import { GroupModule } from './group/group.module';
 import { MessageModule } from './message/message.module';
 
 @Module({
-  imports: [UsersModule, ChatModule, UserModule, GroupModule, MessageModule],
+  imports: [UsersModule, MongooseModule.forRoot('mongodb://localhost/nest')],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
