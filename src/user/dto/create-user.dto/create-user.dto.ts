0c01ac42b4b4ef093c3d77533911da99b15eba56
Toseef Ahmed <tauseefahmed92b@gmail.com>
2024-04-19 11:59:36 +0300

    user swagger fixed

--- src/user/dto/create-user.dto/create-user.dto.ts
+++ src/user/dto/create-user.dto/create-user.dto.ts
@@ -1,16 +1,31 @@
-/* eslint-disable prettier/prettier */
 // create-user.dto.ts
 import { IsString, IsNotEmpty, IsEmail } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'Email address of the user',
+    example: 'john.doe@example.com',
+    required: true,
+  })
   @IsEmail()
   @IsNotEmpty()
   readonly email: string;
 
+  @ApiProperty({
+    description: 'Username of the user',
+    example: 'johndoe',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   readonly username: string;
-  
+
+  @ApiProperty({
+    description: 'Password for the user account',
+    example: 'Secure*1234',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   password: string;
--- src/user/dto/update-user.dto/update-user.dto.ts
+++ src/user/dto/update-user.dto/update-user.dto.ts
@@ -1,39 +1,69 @@
-import {
-  IsEmail,
-  IsEnum,
-  IsOptional,
-  IsString,
-  IsUrl,
-  MaxLength,
-  MinLength,
-} from 'class-validator';
+import { IsEmail, IsEnum, IsOptional, IsString, IsUrl } from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { UserStatus } from '../../schemas/user.schema';
 
 export class UpdateUserDto {
+  @ApiPropertyOptional({
+    description: 'Email address of the user',
+    example: 'john.doe@example.com',
+    type: 'string',
+    format: 'email',
+  })
   @IsOptional()
   @IsEmail()
   readonly email?: string;
 
+  @ApiPropertyOptional({
+    description: 'Password for the user account',
+    example: 'yourStrong(!)Password',
+    type: 'string',
+  })
   @IsOptional()
   @IsString()
   readonly password?: string;
 
+  @ApiPropertyOptional({
+    description: 'First name of the user',
+    example: 'John',
+    type: 'string',
+  })
   @IsOptional()
   @IsString()
   readonly firstName?: string;
 
+  @ApiPropertyOptional({
+    description: 'Last name of the user',
+    example: 'Doe',
+    type: 'string',
+  })
   @IsOptional()
   @IsString()
   readonly lastName?: string;
 
+  @ApiPropertyOptional({
+    description: 'Username of the user',
+    example: 'johndoe',
+    type: 'string',
+  })
   @IsOptional()
   @IsString()
   readonly username?: string;
 
+  @ApiPropertyOptional({
+    description: "URL of the user's profile picture",
+    example: 'http://example.com/your-profile-picture.jpg',
+    type: 'string',
+    format: 'url',
+  })
   @IsOptional()
   @IsUrl()
   profilePicture?: string;
 
+  @ApiPropertyOptional({
+    description: 'Current status of the user',
+    example: UserStatus.Offline,
+    enum: UserStatus,
+  })
   @IsOptional()
   @IsEnum(UserStatus)
   status?: UserStatus;
